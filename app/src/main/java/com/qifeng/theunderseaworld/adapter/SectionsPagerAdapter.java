cd4c2af7f25c1402b96d22e2485ab44888a76091
wu <wu>
2017-03-15 18:36:55 +0800

    登录页面部分布局的实现

---
+++ app/src/main/java/com/qifeng/theunderseaworld/adapter/SectionsPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.qifeng.theunderseaworld.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    List<Fragment> fragments;
+
+    public SectionsPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+}
---
+++ app/src/main/java/com/qifeng/theunderseaworld/view/ImageViewPlus.java
@@ -0,0 +1,10 @@
+package com.qifeng.theunderseaworld.view;
+
+import android.widget.ImageView;
+
+/**
+ * Created by liu on 2017/3/15.
+ */
+
+public class ImageViewPlus extends ImageView {
+}
